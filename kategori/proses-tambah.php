399a28fff0728c491500fcbf76305d308da94820
FaisyaAnindya <146405245+FaisyaAnindya@users.noreply.github.com>
2024-04-20 07:34:13 +0700

    Update proses-tambah.php

--- kategori/proses-tambah.php
+++ kategori/proses-tambah.php
@@ -1,22 +1,17 @@
 <?php
 if ($_POST) {
     include '../config/Database.php' ;
-    include '../object/Petugas.php' ;
+    include '../object/Kategori.php' ;
 
     $database = new Database();
     $db = $database->getConnection();
 
-    $petugas = new petugas ($db);
+    $kategori = new Kategori ($db);
 
-    $petugas->NamaPetugas = $_POST['namapetugas'];
-    $petugas->Alamat = $_POST['alamat'];
-    $petugas->NoTelp = $_POST['notelp'];
-    $petugas->Email = $_POST['email'];
-    $petugas->Password = $_POST['password'];
-    $petugas->Role = $_POST['role'];
+    $kategori->NamaKategori = $_POST['namakategori'];
 
-    $petugas->create();
+    $kategori->create();
 }
 
-header("Location: http://localhost/perpus_app/petugas/index.php");
+header("Location: http://localhost/perpus_app/kategori/index.php");
 ?>
